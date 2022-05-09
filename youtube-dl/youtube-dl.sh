9fac8726d26a61dcf4928677a82c5f14a6385013
Afreto <kong5664546498@gmail.com>
2022-05-10 00:10:40 +0800

    new

---
+++ youtube-dl/youtube-dl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for file in "afreto"
+
+# youtube-dl https://www.youtube.com/watch\?v\=YxkTKb12QZQ\&list\=PLHOL1w6t2NFPYUwYl3vr2coMolzb0Ynf4\&index\=4 -w --external-downloader aria2c --external-downloader-args "-x 16"
