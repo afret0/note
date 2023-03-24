f0f907bd39248ee49bb4c31d42daf64a4ebd7f84
Afreto <kong5664546498@gmail.com>
2023-03-24 18:38:26 +0800

    feat add

--- youtube-dl/youtube-dl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for file in "afreto"
-
-# youtube-dl https://www.youtube.com/watch\?v\=YxkTKb12QZQ\&list\=PLHOL1w6t2NFPYUwYl3vr2coMolzb0Ynf4\&index\=4 -w --external-downloader aria2c --external-downloader-args "-x 16"
